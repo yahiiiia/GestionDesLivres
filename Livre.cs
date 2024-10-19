c3bef1688df97b5c2795fb8d2ec672450998286b
abderrafia <abderrafia.yahia@monboreal.ca>
2024-10-19 18:40:11 -0400

    Ajout de la classe Livre et des fichiers de base du projet

---
+++ Livre.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        // Varaibles pour les info du livre
+        public string tittre; // nom du livre
+        public string auteur; // auteur du livre
+        public double prix;   // le prix en CAD
+
+        // Constrecteur pour cree un livre
+        public Livre(string t, string a, double p)
+        {
+            // Initilisation des varaibles
+            tittre = t;
+            auteur = a;
+            prix = p;
+        }
+
+        // Methode pour aficher les info du livre
+        public void Afficher()
+        {
+            Console.WriteLine("Titre : " + tittre);
+            Console.WriteLine("Auteur : " + auteur);
+            Console.WriteLine("Prix : " + prix + " CAD");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using AppConsoleLivres;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Création d'un nouvel objet Livre
+        Livre livre1 = new Livre("Programmation C#", "Youness Boukouchi", 49.99);
+
+        // Affichage des informations du livre
+        livre1.Afficher();
+
+        // Attendre que l'utilisateur appuie sur une touche pour fermer
+        Console.WriteLine("Appuyez sur une touche pour fermer...");
+        Console.ReadKey();
+    }
+}
